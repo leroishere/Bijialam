c707b2423ef25b5f3bd31055e2121a882f636150
leroishere <liolio141220@gmail.com>
2026-01-29 13:56:19 +0700

    wip: implement router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -8,24 +8,47 @@ use App\Controllers\StudentController;
 
 class Router
 {
+private $routes = [];
+
+
+
+public function add(string $method, string $uri, string $controller, string $function): void
+{
+    $this->routes[] = [
+        'method' => $method,
+        'uri' => $uri,
+        'controller' => $controller,
+        'function' => $function,
+    ];
+}
 public function run(): void
 {
     $method = $_SERVER['REQUEST_METHOD'];
     $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
 
+    foreach ($this->routes as $route) {
+        if ($route['method'] == $method && $route['uri'] == $uri) {
+            $pattern = str_replace(
+                search: '{id}',
+                replace: '([0-9]+)',
+                subject: $route['uri']
+            );
 
-    if ($method == 'GET' && $uri == '/students'){
-       require_once './app/controllers/StudentController.php';
-         $controller = new StudentController();
-         $controller->index();
-        return;
-    }  
- if ($method == 'GET' && $uri == '/students/create'){
-        require_once './app/controllers/StudentController.php';
-         $controller = new StudentController();
-         $controller->create();
-        return;
- }
+            $pattern = '#^' . $pattern . '$#';
+
+            if (preg_match($pattern, $uri, $matches)) {
+                require_once './app/controllers/' . $route['controller'] . '.php';
+                array_shift($matches);
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+
+                $function = $route['function'];
+                $controller->$function();
+
+                return;
+            }
+        }
+    }
 
 
     http_response_code(404);
--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 require_once './app/core/Router.php';
 use App\Core\Router;
 $router = new Router();
+
+//register routes
+$router->add(method: 'GET', uri: '/students', controller: 'StudentController', function: 'index');
+$router->add(method: 'GET', uri: '/students/create', controller: 'StudentController', function: 'create');
+$router->add(method: 'GET', uri: '/students/{id}', controller: 'StudentController', function: 'show');
 $router->run();
 ?>
  
