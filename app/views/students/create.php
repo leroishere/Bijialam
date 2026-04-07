5d1d8c401a6554fd5fa73ce461307602effe5888
leroishere <liolio141220@gmail.com>
2026-04-07 12:24:32 +0700

    feat: implement submit button and back button in student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -3,11 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah siswa</title>
+    <title>Tambah</title>
+    <link rel = "stylesheet" href="/css/output.css">
 </head>
-
-<body>
-    <h1>Tambah Siswa</h1>
-    <p>Menamplikan form tambah siswa</p>
+ 
+<body class="min-h-screen flex flex-col bg-gray-100">
+    <header class="bg-gradient-to-r from-blue-600 to-blue-800 text-white shadow-lg">    
+        <div class="flex justify-between items-center container mx-auto py-6 px-4">
+            <a href="/students" class="font-bold text-2xl hover:text-blue-200 transition">Sistem Sekolah</a>
+            <a href="/students/create" class="bg-white text-blue-600 px-6 py-2 rounded-lg font-semibold hover:bg-blue-50 transition shadow-md">+ Tambah Siswa</a>
+        </div>
+    </header>
+ 
+    <main class="container mx-auto grow">
+    <div class="mt-8 space-y-2">
+        <div class="p-4 shadow rounded-lg bg-white">
+            <h1 class="text-2xl font-bold">Tambah Siswa</h1>
+            <p>Menambahkan siswa kedalam </p>
+    </div>
+ 
+    <div class="p-4 bg-white shadow rounded-lg">
+       <form action="" class="grid grid-cols-2 gap-4">
+        <div class="space-y-2">
+            <label class="block font-bold" for="name">Nama</label>
+            <input class="w-full px-4 py-2 border rounded-lg" type="text"  id="name" class="w-full border rounded-lg p-2">
+        </div>
+        <div class="space-y-2">
+            <label class="block font-bold" for="class">Kelas</label>
+            <input class="w-full px-4 py-2 border rounded-lg" type="text"  id="name" class="w-full border rounded-lg p-2">
+        </div>
+        <div class="space-y-2">
+            <label class="block font-bold" for="NIS">NIS</label>
+            <input class="w-full px-4 py-2 border rounded-lg" type="text"  id="name" class="w-full border rounded-lg p-2">
+        </div>
+        <div class="space-y-2">
+            <label class="block font-bold" for="phone_number">Nomor Telepon</label>
+            <input class="w-full px-4 py-2 border rounded-lg" type="text"  id="name" class="w-full border rounded-lg p-2">
+        </div>
+        <div class ="flex justify-end col-span-2 gap-4">
+             <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+        <button type="submit" class="px-4 py-2 bg-blue-500 rounded-lg text-white">Simpan</button>
+        </div>
+       </form>
+    </div>
+ 
+        </div>
+    </main>
+ 
+    <footer class="bg-gray-600 text-white">
+        <div class="text-center p-4">
+            &copy <?= date('Y')?> Sistem Sekolah - SMK Kristen Immanuel
+        </div>
+    </footer>
 </body>
-</html>
--- app/views/students/index.php
+++ app/views/students/index.php
@@ -15,54 +15,50 @@
         </div>
     </header>
  
-     <main class="container mx-auto grow">
-    <div class="mt-8 space-y-2">
-        <div class="p-4 shadow rounded-lg bg-white">
-            <h1 class="text-2xl font-bold ">Daftar Siswa</h1>
-            <p>Menampilkan data siswa yang terdaftar</p>
-    </div>
- 
-    <div class="bg-white shadow rounded-lg">
-        <table class= "w-full">
-            <thead class="bg-gray-200">
-                <tr>
-                <th class="px-4 py-2 text-left">No</th>
-                <th class="px-4 py-2 text-left">Nama</th>
-                <th class="px-4 py-2 text-left">NIS</th>
-                <th class="px-4 py-2 text-left">Kelas</th>
-                <th class="px-4 py-2 text-left">No Telepon</th>
-                <th class="px-4 py-2">Aksi</th>
-            </tr>
-        </thead>
-            <tbody>
-                <tr>
-                    <td class="px-4 py-2 text-left">1</td>
-                    <td class="px-4 py-2 text-left">Darill</td>
-                    <td class="px-4 py-2 text-left">1234</td>
-                    <td class="px-4 py-2 text-left">XI TKJ 2</td>
-                    <td class="px-4 py-2 ">085849264487</td>
-                    <td class="px-4 py-2">
-                        <div class= "flex justify-center items-center gap-2">
-                            <a href="" class= "text-green-500">Detail</a>
-                            <a href="" class= "text-yellow-500">Edit</a>
-                            <a href="" class= "text-red-500">Hapus</a>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
- 
+    <main class="container mx-auto grow">
+        <div class="mt-8 space-y-2">
+            <div class="p-4 shadow rounded-lg bg-white">
+                <h1 class="text-2xl font-bold ">Daftar Siswa</h1>
+                <p>Menampilkan data siswa yang terdaftar</p>
+            </div>
+       
+        <div class="p-4 bg-white shadow rounded-lg">
+            <table class="w-full">
+                <thead class="bg-gray-200">
+                    <tr>
+                        <th class="px-4 py-2 text-left">Nomor</th>
+                        <th class="px-4 py-2 text-left">Nama</th>
+                        <th class="px-4 py-2 text-left">NIS</th>
+                        <th class="px-4 py-2 text-left">Kelas</th>
+                        <th class="px-4 py-2 text-left">No Telepon</th>
+                        <th class="px-4 py-2">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="px-4 py-2 text-left">1</td>
+                        <td class="px-4 py-2 text-left">Andi</td>
+                        <td class="px-4 py-2 text-left">1234</td>
+                        <td class="px-4 py-2 text-left">11 TKJ 2</td>
+                        <td class="px-4 py-2 text-left">08123456789</td>
+                        <td>
+                            <div class="flex justify-center items-center gap-4">
+                                <a href="" class="text-green-500">Detail</a>
+                                <a href="" class="text-yellow-500">Edit</a>
+                                <a href="" class="text-red-500">Hapus</a>
+                            </div>  
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </main>
  
-    <footer class="bg-gray-600 text-white">
-        <div class="text-center p-4">class= "text-green-500"
+ 
+    <footer class="bg-gray-800 text-white">
+        <div class="text-center p-4">
             &copy <?= date('Y')?> Sistem Sekolah - SMK Kristen Immanuel
         </div>
     </footer>
 </body>
 </html>
-
-
-<div class= "bg-white shadow rounded">
